7d346ba005a951cc6543b07da9197f761adabd64
Eneko Galan <enekogalanelorza@gmail.com>
2025-03-09 19:42:48 +0100

    Tables hack for default options

--- src/js/easymde.js
+++ src/js/easymde.js
@@ -1119,11 +1119,13 @@ function parseHTML(htmlContent, editor) {
     turndownService.use(gfm);
     var markdown = turndownService.turndown(htmlContent);
     markdown = markdown.replaceAll(newLineChar, ''); // New lines hack
-    markdown = markdown.replaceAll(/~(.*?)~/g, '~~$1~~'); // ~something~ to ~~something~~ 
+    markdown = markdown.replaceAll(/~(.*?)~/g, '~~$1~~'); // from ~something~ to ~~something~~ 
     markdown = markdown.replace(new RegExp(`([^\n])\\n{2,}(${blockStyles.code}[\\s\\S]+?${blockStyles.code})`, 'g'), '$1\n$2'); // Remove blank lines before a code block if there is text before
     markdown = markdown.replace(new RegExp(`\\n+(${blockStyles.code}\\w*\\n[\\s\\S]+?${blockStyles.code})`, 'g'), '\n$1'); // Remove empty lines between consecutive code blocks
     markdown = markdown.replace(new RegExp(`(${blockStyles.code}[\\s\\S]+?${blockStyles.code})\\n{2,}([^\n])`, 'g'), '$1\n$2'); // Remove extra line break between last code block and subsequent text
     markdown = markdown.replace(/\\\*/g, '*'); // Remove any escaping from asterisks if they are in Markdown contexts
+    markdown = markdown.replace(/(\|.*\|(\n\|.*\|)*)/g, '\n$1\n'); // Replace tables by adding line breaks at the beginning and end (Only works with default insertTexts.table value)
+    markdown = markdown.replace(/\| --- \| --- \| --- \|/, '| -------- | -------- | -------- |'); // tables row from --- to --------
     return markdown;
 }
 
